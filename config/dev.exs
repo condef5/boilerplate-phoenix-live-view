e37813a8c025566a4d847e70b8c9ffe6d07d934d
condef5 <frank@able.co>
2019-12-22 17:55:09 -0500

    Add extra configurations

--- config/dev.exs
+++ config/dev.exs
@@ -61,7 +61,8 @@ config :live_view_todos, LiveViewTodosWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/live_view_todos_web/{live,views}/.*(ex)$",
-      ~r"lib/live_view_todos_web/templates/.*(eex)$"
+      ~r"lib/live_view_todos_web/templates/.*(eex)$",
+      ~r{lib/live_view_todos_web/live/.*(ex)$}
     ]
   ]
 
