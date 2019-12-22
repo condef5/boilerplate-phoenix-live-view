f0998167ea8e5698ad56551217240f06b2abd799
condef5 <frank@able.co>
2019-12-22 13:57:53 -0500

    Add space page

--- lib/live_view_todos_web/router.ex
+++ lib/live_view_todos_web/router.ex
@@ -18,6 +18,7 @@ defmodule LiveViewTodosWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    live "/space", SpaceLive
   end
 
   # Other scopes may use custom stacks.
